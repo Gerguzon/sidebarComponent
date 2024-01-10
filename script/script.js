09bbad9e976affd80ed9656dcbad15e05f780b4d
German Ramirez <61391387+Gerguzon@users.noreply.github.com>
2024-01-10 14:30:03 -0800

    Proyecto completo sidebar
    Hice el sidebar completo con la interacción, con lo que se de JS

---
+++ script/script.js
@@ -0,0 +1,9 @@
+var hamburgerIcon = document.getElementById("hamburgerIcon");
+var sidebarSmall = document.getElementById("sidebarSmall");
+var sidebarComplete = document.getElementById("sidebarComplete");
+
+
+function miFunc() {
+    sidebarSmall.classList.toggle("display");
+    sidebarComplete.classList.toggle("display");
+}
